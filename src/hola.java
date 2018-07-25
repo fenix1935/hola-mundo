4e9bae66046138f9e94b9d436c06d5d4a660e7e4
LabingXEON <LabingXEON@LabingXEON22-PC>
2018-07-25 11:20:09 -0500

    version 1.0 de hola mundo

---
+++ src/hola.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sergio ivan peñalosa rojas
+ */
+public class hola {
+    public static void main(String[] args) {
+        System.out.println("hola Mundo Cruel :v");
+    }
+    
+}
